9008de81e7451010e7277e0c6d5ce438a69c6d05
Harascho1 <luka.old1@gmail.com>
2025-02-23 19:56:08 +0100

    jako

--- dec.h
+++ dec.h
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define true 1
+#define false 0
+
+// Constants
+const int g_next_right = 1;
+const int g_next_down = 2;
+
+// Structs
 typedef struct rect_t {
     int X, Y, Width, Height;
 } rect_t;
@@ -11,9 +19,24 @@ typedef struct pice_t {
     int X, Y;
     int Width, Height;
     bool Set;
+    int Next;
+
 } pice_t;
 
-int intersect(rect_t pos, pice_t pice);
+typedef struct cut_t
+{
+    int X, Y;
+    int Width, Height;
+} cut_t;
+
+typedef struct cut_array_t {
+    cut_t *cuts;
+    int num_of_cuts;
+} cut_array_t;
+
+// Declarations
+int intersect(rect_t pos, pice_t pice, pice_t *array);
+int get_area(pice_t pice);
 void print(pice_t *array);
 void print_pos(rect_t *free_pos, int count);
 void rotate(pice_t *pice);
@@ -21,4 +44,13 @@ bool is_square(pice_t pice);
 void sort();
 void copy_to_array(pice_t *copy_to, pice_t *copy_from);
 void copy_to_pos(rect_t *copy_to, rect_t* copy_from);
+void copy_to_cut(cut_array_t *copy_to, cut_array_t *copy_from);
 bool out_of_bounds(rect_t free_pos, pice_t pice);
+int get_full_area(pice_t *pices_array, int count);
+void sort_array(pice_t *array, int count);
+int max_area(pice_t *pices_array);
+bool intersect_with_cut(cut_array_t *array, pice_t pice);
+int make_cut(cut_array_t *cut_array, pice_t *pices_array, pice_t *pice, rect_t *pos);
+void free_cuts(cut_array_t *cut_array);
+bool is_pices_equa(pice_t pice1, pice_t pice2);
+void print_cuts(cut_array_t *cuts);
--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 pice_t *pices_array = NULL;
 pice_t *final_array = NULL;
 int MAX_AREA = 0;
-int pices_num = 5;
-const int WIDTH = 500;
-const int HEIGHT = 700;
+int FULL_AREA = 0;
+int PICES_NUM;
+int WIDTH;
+int HEIGHT;
 
 int get_area(pice_t pice) {
     return pice.Width * pice.Height;
@@ -13,7 +14,7 @@ int get_area(pice_t pice) {
 
 int evaluate(pice_t *pices) {
     int sum = 0;
-    for (int i = 0; i < pices_num; i++) {
+    for (int i = 0; i < PICES_NUM; i++) {
         if (pices[i].Set == true) {
             sum += get_area(pices_array[i]);
         }
@@ -22,18 +23,19 @@ int evaluate(pice_t *pices) {
     return sum;
 }
 
-int succsesors(pice_t *pice, rect_t *free_pos, int *count, int* p) {
+int successors(pice_t *pice, rect_t *free_pos, int *count, int* p, pice_t *pices) {
     int tmp = *count;
+    if (pice->Set == true) {
+        return -1;
+    }
     for (int i = *p; i < tmp; i++) {
-        if (intersect(free_pos[i], *pice) == true) {
-            printf("Parce [%d, %d] se intersektuje\n", pice->Width, pice->Height);
-            continue;
-        }
+
         if (out_of_bounds(free_pos[i], *pice) == true) {
-            printf("Parce [%d, %d] je out of bounds na poziciji (%d, %d), jer je slobodna dimenzija [%d, %d]\n", 
-                pice->Width, pice->Height, free_pos[i].X, free_pos[i].Y, free_pos[i].Width, free_pos[i].Height);
+            // printf("Parce [%d, %d] je out of bounds na poziciji (%d, %d), jer je slobodna dimenzija [%d, %d]\n", 
+            //     pice->Width, pice->Height, free_pos[i].X, free_pos[i].Y, free_pos[i].Width, free_pos[i].Height);
             continue;
         }
+
         pice->Set = true;
         pice->X = free_pos[i].X;
         pice->Y = free_pos[i].Y;
@@ -44,152 +46,222 @@ int succsesors(pice_t *pice, rect_t *free_pos, int *count, int* p) {
         int width = pice->Width;
         int height = pice->Height;
 
+        // make_cut(cuts, pices, pice, &free_pos[i]);
+
         free_pos[(*count)++] = (rect_t){old_x + width, old_y, free_pos[i].Width - width, free_pos[i].Height};
-        free_pos[i] = (rect_t){old_x, old_y + height, free_pos[i].Width, free_pos[i].Height- height};
-        printf("Stavljena je plocica na mestu (%d, %d), dimenzije [%d, %d]\n",
-            x, y, width, height);
-        print_pos(free_pos, *count);
+        free_pos[i] = (rect_t){old_x, old_y + height, width, free_pos[i].Height- height};
+
         (*p) = i + 1;
-        break;
+        return 1;
     }
-    if (*count == tmp) {
-        return -1;
-    }
-    return 1;
+    // printf("Nema vise slobodnih mesta za postavljanje parceta\n");
+    return -1;
 }
 
-int intersect(rect_t pos, pice_t pice) {
+int make_cut(cut_array_t *cut_array, pice_t *pices, pice_t *pice, rect_t *free_pos) {
+    if (cut_array->num_of_cuts == -1) {
+        printf("Postavilo se prvo parce\n");
+        cut_array->num_of_cuts = 0;
+        return 0;
+    }
 
-    for (int i = 0; i < pices_num; i++) {
-        int x = pos.X;
-        int width = pos.X + pice.Width;
-        int y = pos.Y;
-        int height = pos.Y + pice.Height;
-
-        if (pices_array[i].Set == true) {
-            int tmp_x = pices_array[i].X;
-            int tmp_width = pices_array[i].X + pices_array[i].Height;
-            int tmp_y = pices_array[i].Y;
-            int tmp_height = pices_array[i].Height + tmp_y;
-
-            // provera sirine
-            if ((tmp_x > x && tmp_x < width) &&
-                (y >= tmp_y && y <= tmp_height)) {
-                printf("Intersektuje se u sirini sa parcetom koji se nalazi na (%d, %d) i velicine [%d, %d]\n",
-                    tmp_x, tmp_y, pices_array[i].Width, pices_array[i].Height);
-                return 1;
-            }
-            //provera visine
-            if ((tmp_y > y && tmp_y < height) &&
-                (x >= tmp_x && x <= tmp_width)) {
-                printf("Intersektuje se u visini sa parcetom koji se nalazi na (%d, %d) i velicine [%d, %d]\n",
-                    tmp_x, tmp_y, pices_array[i].Width, pices_array[i].Height);
-                return 1;
-            }
+    // Provera da li je vec na istoj liniji na kojoj je postavljen CUT
+    for (int i = 0; i < cut_array->num_of_cuts; i++) {
+        if (cut_array->cuts[i].X == pice->X || cut_array->cuts[i].Y == pice->Y) {
+            printf("Vec je postavljen cut na poziciji (%d, %d)\n", pice->X, pice->Y);
+            return 0;
         }
     }
+
+    for (int i = 0; i < PICES_NUM; i++) {
+
+        if (pices[i].Set == false) {
+            continue;
+        }
+
+        if (pices[i].Next == 3) {
+            continue;
+        }
+
+        if (is_pices_equa(pices[i], *pice) == true) {
+            continue;
+        }
+
+        // Provera da li je pice postavljen DESNO od vec postavljenog parceta
+        if (pices[i].X + pices[i].Width == pice->X) {
+            if (pices[i].Next == 0) {
+                printf("Pice je postavljeno desno od vec postavljenog parceta\n");
+                cut_array->cuts[cut_array->num_of_cuts++] = (cut_t){pice->X, pice->Y, pice->X, free_pos->Height};
+            } else {
+                cut_array->cuts[cut_array->num_of_cuts++] = (cut_t){pice->X, pice->Y, pice->X, pices[i].Height};
+            }
+            pices[i].Next += g_next_right;
+            break;
+        // Provera da li je pice postavljen ISPOD od vec postavljenog parceta
+        } else if (pices[i].Y + pices[i].Height == pice->Y){
+            if (pices[i].Next == 0) {
+                cut_array->cuts[cut_array->num_of_cuts++] = (cut_t){pice->X, pice->Y, free_pos->Width, pice->Y};
+            } else {
+                cut_array->cuts[cut_array->num_of_cuts++] = (cut_t){pice->X, pice->Y, pices[i].Width, pice->Y};
+            }
+            pices[i].Next += g_next_down;
+            break;
+        }
+    }
+    print_cuts(cut_array);
     return 0;
+
 }
 
-//* algo
-void algo(pice_t *pices, int depth, rect_t *free_pos, int count) {
-
-    // * pravi se free_pos
-    // if (free_pos == NULL && depth == pices_num) {
-    //     free_pos = (rect_t*)malloc(sizeof(rect_t)*pices_num * 2);
-    //     free_pos[0] = (rect_t){0, 0, WIDTH, HEIGHT};
-    // }
-    // if (pices == NULL && depth == pices_num) {
-    //     pices = (pice_t*)malloc(sizeof(pice_t)*pices_num);
-    //     for (int i = 0; i < pices_num; i++) {
-    //         pices[i] = pices_array[i];
-    //         //pices[i] = (pice_t){pices_array[i].X, pices_array[i].Y, pices_array[i].Width, pices_array[i].Height, pices_array[i].Set};
-    //     }
-    // }
-
-    printf("ALGO JE SA DEPTHOM = %d\n", depth);
-    if (depth == 0) {
-        int tmp = evaluate(pices);
-        if (tmp > MAX_AREA) {
-            printf("Dobio sam final_array\n");
-            copy_to_array(final_array, pices);
-        }
+void print_cuts(cut_array_t *cuts) {
+    printf("-----------------------------------\n");
+    printf("Niz cut-ova:\n\n");
+    if (cuts->num_of_cuts == -1) {
+        printf("Nema cut-ova\n");
         return;
     }
-    
-
-    for (int i = 0; i < pices_num; i++) {
-        int p = 0;
-        if (pices[i].Set == false) {
-            printf("(%d, %d) i [%d, %d]\n", pices[i].X, pices[i].Y, pices[i].Width, pices[i].Height);
-            int tmp = count;
-            pice_t *tmp_pices;
-            rect_t *tmp_pos;
-            // Set pice
-            while (succsesors(&pices[i], free_pos, &count, &p) != -1) {
-                print(pices);
-                tmp_pices = (pice_t*)malloc(sizeof(pice_t) * pices_num);
-                tmp_pos = (rect_t*)malloc(sizeof(rect_t) * pices_num * 2);
-                copy_to_pos(tmp_pos, free_pos);
-                copy_to_array(tmp_pices, pices);
-                printf("pozvao sam algo sa depthom = %d\n", depth-1);
-                algo(tmp_pices, depth - 1, tmp_pos, count);
-                copy_to_array(pices, tmp_pices);
-                copy_to_pos(free_pos, tmp_pos);
-                count = tmp;
-            }
-            free(tmp_pices);
-            free(tmp_pos);
-        }
+    for (int i = 0; i < cuts->num_of_cuts; i++) {
+        printf("Cut: (%d, %d) i dimenzije [%d, %d]\n", 
+            cuts->cuts[i].X, cuts->cuts[i].Y, cuts->cuts[i].Width, cuts->cuts[i].Height);
     }
-    return;
-
+    printf("-----------------------------------\n");
 }
 
-int main() {
-    pices_num = 5;
 
-    pices_array = (pice_t*)malloc(sizeof(pice_t)*pices_num);
-    final_array = (pice_t*)malloc(sizeof(pice_t)*pices_num);
-
-    pice_t *pices = NULL;
-    rect_t *free_pos = NULL;
-    pices_array[0] = (pice_t){0, 0, 250, 100, false};
-    pices_array[1] = (pice_t){0, 0, 100, 100, false};
-    pices_array[2] = (pice_t){0, 0, 300, 50, false};
-    pices_array[3] = (pice_t){0, 0, 250, 500, false};
-    pices_array[4] = (pice_t){0, 0, 400, 20, false};
+void algo(pice_t *pices, int depth, rect_t *free_pos, int count) {
 
 
-    for (int i = 0; i < pices_num; i++) {
-        free_pos = (rect_t*)malloc(sizeof(rect_t)*pices_num * 2);
-        free_pos[0] = (rect_t){0, 0, WIDTH, HEIGHT};
-
-        pices = (pice_t*)malloc(sizeof(pice_t)*pices_num);
-        copy_to_array(pices,pices_array);
-        algo(&pices[i], pices_num, free_pos, 1);
-        // free(pices);
+    if (MAX_AREA == FULL_AREA) {
+        return;
     }
 
-    printf("ukupna povrsina je: %d\n", evaluate(final_array));
+    if (depth == 0) {
+        max_area(pices);
+        return;
+    }
+
+    for (int i = 0; i < PICES_NUM; i++) {
+        if (pices[i].Set == true) {
+            continue;
+        }   
+        //! printf("(%d, %d) i [%d, %d]\n", pices[i].X, pices[i].Y, pices[i].Width, pices[i].Height);
+        int tmp = count;
+        pice_t *tmp_pices;
+        tmp_pices = (pice_t*)malloc(sizeof(pice_t) * PICES_NUM);
+        rect_t *tmp_pos;
+        tmp_pos = (rect_t*)malloc(sizeof(rect_t) * PICES_NUM + 1);
+
+        copy_to_array(tmp_pices, pices);
+        copy_to_pos(tmp_pos, free_pos);
+        // Try placing the piece without rotation
+        int p = 0;
+        while (successors(&pices[i], free_pos, &count, &p, pices) != -1) {
+            //print(pices);
+            //! printf("pozvao sam algo sa depthom = %d\n", depth-1);
+            algo(pices, depth - 1, free_pos, count);
+            count = tmp;
+            p = 0;
+        }
+        // Try placing the piece with rotation
+        if (is_square(pices[i]) == false) {
+            copy_to_array(pices, tmp_pices);
+            copy_to_pos(free_pos, tmp_pos);
+
+            rotate(&pices[i]);
+            while (successors(&pices[i], free_pos, &count, &p, pices) != -1) {
+                //print(pices);
+                //! printf("pozvao sam algo sa depthom = %d\n", depth-1);
+                algo(pices, depth - 1, free_pos, count);
+                count = tmp;
+                p = 0;
+            }
+            // Restore the original state
+            rotate(&pices[i]); // Rotate back to original orientation
+        }
+        copy_to_array(pices, tmp_pices);
+        copy_to_pos(free_pos, tmp_pos);
+        //copy_to_cut(cuts, tmp_cuts);
+        free(tmp_pices);
+        free(tmp_pos);
+    }
+    max_area(pices);
+    return;
+}
+int main() {
+    pice_t *pices;
+    rect_t *free_pos;
+    cut_array_t *cuts;
+
+    FILE *file = fopen("primer5.txt", "r");
+
+    while(feof(file) == 0) {
+        fscanf(file, "%d %d", &WIDTH, &HEIGHT);
+        fscanf(file, "%d", &PICES_NUM);
+        pices_array = (pice_t*)malloc(sizeof(pice_t)*PICES_NUM);
+        final_array = (pice_t*)malloc(sizeof(pice_t)*PICES_NUM);
+        for (int i = 0; i < PICES_NUM; i++) {
+            fscanf(file, "%d %d", &pices_array[i].Width, &pices_array[i].Height);
+            pices_array[i].Set = false;
+            pices_array[i].Next = 0;
+        }
+    }
+
+    FULL_AREA = get_full_area(pices_array, PICES_NUM);
+    printf("FULL AREA = %d\n", FULL_AREA);
+
+    //TODO NE ZNAM KAKO OVO DA RESIM
+    int possible_area = WIDTH * HEIGHT;
+    //! if (FULL_AREA > possible_area) {
+    //!     printf("Nemoguce je postaviti sve parcice\n");
+    //!     return 0;
+    //! }
+
+    free_pos = (rect_t*)malloc(sizeof(rect_t)*PICES_NUM * 2);
+    free_pos[0] = (rect_t){0, 0, WIDTH, HEIGHT};
+
+    pices = (pice_t*)malloc(sizeof(pice_t)*PICES_NUM);
+    copy_to_array(pices, pices_array);
+    sort_array(pices, PICES_NUM);
+
+
+    algo(pices, PICES_NUM, free_pos, 1);
+
+    printf("Ukupna povrsina je: %d\n", evaluate(final_array));
+    print(final_array);
 
     free(pices);
     free(free_pos);
 
     free(pices_array);
     free(final_array);
-
-
-
-    //print(final_array);
-    
     return 0;
+}
+
+void sort_array(pice_t *array, int count) {
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = i + 1; j < count; j++) {
+            if (get_area(array[j]) > get_area(array[i])) {
+                pice_t tmp = array[j];
+                array[j] = array[i];
+                array[i] = tmp;
+            }
+        }
+    }
+}
+
+int max_area(pice_t *pices) {
+    int tmp = evaluate(pices);
+    if (tmp > MAX_AREA) {
+        MAX_AREA = tmp;
+        copy_to_array(final_array, pices);
+        printf("Nadjena je nova max povrsina = %d\n", MAX_AREA);
+    }
 }
 
 void print(pice_t *array) {
     printf("-----------------------------------\n");
     printf("Niz postavljenih parcica:\n\n");
-    for (int i = 0; i < pices_num; i++) {
+    for (int i = 0; i < PICES_NUM; i++) {
         if (array[i].Set == true) {
             printf("Parce: [%d, %d], postavljeno je (%d, %d) i povrsina je %d\n", 
                 array[i].Width, array[i].Height, array[i].X, array[i].Y, get_area(array[i]));
@@ -209,20 +281,35 @@ void rotate(pice_t *pice) {
 }
 
 void copy_to_array(pice_t *copy_to, pice_t *copy_from) {
-    for (int i = 0; i < pices_num; i++) {
+    for (int i = 0; i < PICES_NUM; i++) {
         copy_to[i] = copy_from[i];
     }
 }
 
+void copy_to_cut(cut_array_t *copy_to, cut_array_t *copy_from) {
+    copy_to->num_of_cuts = copy_from->num_of_cuts;
+    for (int i = 0; i < copy_from->num_of_cuts; i++) {
+        copy_to->cuts[i] = copy_from->cuts[i];
+    }
+}
+
+int get_full_area(pice_t *pices_array, int count) {
+    int sum = 0;
+    for (int i = 0; i < count; i++) {
+        sum += get_area(pices_array[i]);
+    }
+    return sum;
+}
+
 void copy_to_pos(rect_t *copyTo, rect_t *copyFrom) {
-    for (int i = 0; i < pices_num; i++) {
+    for (int i = 0; i < PICES_NUM; i++) {
         copyTo[i] = copyFrom[i];
     }
 }
 
 void sort() {
-    for (int i = 0; i < pices_num- 1; i++) {
-        for (int j = i + 1; j < pices_num; j++) {
+    for (int i = 0; i < PICES_NUM- 1; i++) {
+        for (int j = i + 1; j < PICES_NUM; j++) {
             if (get_area(pices_array[j]) > get_area(pices_array[j])) {
                 pice_t tmp = pices_array[j];
                 pices_array[j] = pices_array[i];
@@ -244,4 +331,59 @@ void print_pos(rect_t *free_pos, int count) {
             free_pos[i].X, free_pos[i].Y, free_pos[i].Width, free_pos[i].Height);
     }
     printf("-----------------------------------\n");
+}
+
+void free_cuts(cut_array_t *cuts) {
+    free(cuts->cuts);
+    free(cuts);
+}
+
+bool is_pices_equa(pice_t pice1, pice_t pice2) {
+    return pice1.X == pice2.X && pice1.Y == pice2.Y && 
+           pice1.Width == pice2.Width && pice1.Height == pice2.Height && 
+           pice1.Set == pice2.Set;
+}
+
+int intersect(rect_t pos, pice_t pice, pice_t * pices_array) {
+
+    int x = pos.X;
+    int width = pos.X + pice.Width;
+    int y = pos.Y;
+    int height = pos.Y + pice.Height;
+
+    for (int i = 0; i < PICES_NUM; i++) {
+        if (pices_array[i].Set == true) {
+            int tmp_x = pices_array[i].X;
+            int tmp_width = pices_array[i].X + pices_array[i].Width;
+            int tmp_y = pices_array[i].Y;
+            int tmp_height = pices_array[i].Y + pices_array[i].Height;
+
+            // Check if the rectangles intersect
+            if (!(width <= tmp_x || x >= tmp_width || height <= tmp_y || y >= tmp_height)) {
+                printf("Intersektuje se sa parcetom koji se nalazi na (%d, %d) i velicine [%d, %d]\n",
+                    tmp_x, tmp_y, pices_array[i].Width, pices_array[i].Height);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool intersect_with_cut(cut_array_t *cuts, pice_t pice) {
+
+
+    for (int i = 0; i < cuts->num_of_cuts; i++) {
+        int x = cuts->cuts[i].X;
+        int width = cuts->cuts[i].Width;
+        int y = cuts->cuts[i].Y;
+        int height = cuts->cuts[i].Height;
+
+        if (pice.X >= x && pice.X <= x + width && pice.Y >= y && pice.Y <= y + height) {
+            printf("Intersektuje se sa cut-om na poziciji (%d, %d) i dimenzijama [%d, %d]\n", x, y, width, height);
+            return true;
+        }
+    }
+
+    return false;
+
 }
